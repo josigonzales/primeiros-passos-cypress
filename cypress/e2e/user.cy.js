a3247d7f7808e46f5eb62883f92a5d02bcff7c1e
josielle <josielle.ro@gmail.com>
2026-08-26 08:11:49 -0300

    User update scenario added

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -9,20 +9,36 @@ describe('Orange HRM Tests', () => {
     loginButton: '.oxd-button',
     wrongCredentialAlert: '.oxd-alert',
     sectionTitleTopBar: '.oxd-topbar-header-breadcrumb > .oxd-text',
+    myInfoButton: "[href='/web/index.php/pim/viewMyDetails']",
+    firstNameField: "[name='firstName']",
+    lastNameField: "[name='lastName']",
+    genericField: '.oxd-input--active',
+    dateField: "[placeholder='yyyy-dd-mm']",
+    dateCloseButton: '.--close',
+    submitButton: "[type='submit']",
   }
 
   
-  it('Login - Sucess', () => {
+  it.only('User Info Update - Sucess', () => {
     cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userSucess.username)
     cy.get(selectorsList.passwordField).type(userData.userSucess.password)
     cy.get(selectorsList.loginButton).click()
     cy.location('pathname').should('equal', '/web/index.php/dashboard/index')
     cy.get('.oxd-topbar-header-breadcrumb > .oxd-text').contains('Dash')
+    cy.get(selectorsList.myInfoButton).click()
+    cy.get(selectorsList.firstNameField).clear().type('FirstName Test')
+    cy.get(selectorsList.lastNameField).clear().type('LastName Test')
+    cy.get(selectorsList.genericField).eq(3).clear().type('ID')
+    cy.get(selectorsList.genericField).eq(4).clear().type('Other ID')
+    cy.get(selectorsList.genericField).eq(5).clear().type('Drivers Number')
+    cy.get(selectorsList.genericField).eq(6).clear().type('2026-26-08')
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.submitButton).eq(0).click()
   })
 
   it('Login - Fail', () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    cy.visit('/auth/login')
     cy.get(selectorsList.usernameField).type(userData.userFail.username)
     cy.get(selectorsList.passwordField).type(userData.userFail.password)
     cy.get(selectorsList.loginButton).click()
